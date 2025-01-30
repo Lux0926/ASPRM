0c814110b1147bbc3a0f789ed0401236bcc9f28f
卢俊杰 <136686972+Lux0926@users.noreply.github.com>
2025-01-31 02:28:10 +0800

    Update run_all_server.sh

--- evaluation/math/TVD/run_all_server.sh
+++ evaluation/math/TVD/run_all_server.sh
@@ -1,14 +1,8 @@
 
 # base model
-bash run_task_server.sh 0 /inspire/hdd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/liuyuliang-240108350135/models/metamath_llama31_8b 8080 /inspire/hdd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/liuyuliang-240108350135/models/metamath_llama31_8b &
+bash run_task_server.sh 0 {task_model_path} 8080 {task_model_path} &
 
 # PRM model
-bash run_reward_server_mistral.sh 1 /inspire/hdd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/liuyuliang-240108350135/S2PRM/OpenRLHF/examples/scripts/checkpoint/s2prm_datamistralv3_modelmistral_bz256_lr1e6_epo1_no_con 8081 &
+bash run_reward_server_mistral.sh 1 {prm_model_path} 8081 &
 
-
-# base model
-bash run_task_server.sh 2 /inspire/hdd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/liuyuliang-240108350135/models/metamath_llama31_8b 8090 /inspire/hdd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/liuyuliang-240108350135/models/metamath_llama31_8b &
-
-# PRM model
-bash run_reward_server_llama.sh 3 /inspire/hdd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/liuyuliang-240108350135/S2PRM/OpenRLHF/examples/scripts/checkpoint/s2prm_datamistralv3_modelllama_bz256_lr2e6_epo1_no_con 8083 &
 
