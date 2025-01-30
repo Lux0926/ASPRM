49fdb6ade628c4e0e90d23f2c4a2257358dbcdce
卢俊杰 <136686972+Lux0926@users.noreply.github.com>
2025-01-31 02:28:50 +0800

    Update run_tvd.sh

--- evaluation/math/TVD/run_tvd.sh
+++ evaluation/math/TVD/run_tvd.sh
@@ -4,7 +4,7 @@ python bon_infer.py \
     --confidence_threshold 55 \
     --task_model_path {task_model_path}  \
     --reward_model_path {prm_model_path} \
-    --input_data_path GSM8k_test_idx.jsonl \
+    --input_data_path {dataset_type} \
     --output_data_path {prm_name}_con55_{task_model_name}.jsonl \
     --task_port 8080 \
     --reward_port 8081 &
